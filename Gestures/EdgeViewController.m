9a99c500ec9eaf35082700a44b30f861a628243e
nekomeo <linh.tu545@gmail.com>
2017-08-06 12:43:39 -0700

    Fix ScreenEdgeGesture to animate with proper rules

--- Gestures/EdgeViewController.m
+++ Gestures/EdgeViewController.m
@@ -12,9 +12,10 @@
 
 @property (nonatomic) UIView *edgeView;
 @property (nonatomic) UIScreenEdgePanGestureRecognizer *edgePanGesture;
-@property (nonatomic) UIPanGestureRecognizer *swipeRightGesture;
-@property (nonatomic) CGFloat width;
-@property (nonatomic) CGFloat height;
+@property (nonatomic) UIPanGestureRecognizer *panGesture;
+
+@property (nonatomic) CGFloat edgeViewOldMidX;
+@property (nonatomic) CGFloat edgeViewNewMidX;
 
 @end
 
@@ -22,67 +23,55 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    self.width = 400;
-    self.height = 400;
+    CGFloat width = 400;
+    CGFloat height = 400;
     
-    CGRect frame = CGRectMake(CGRectGetMidX(self.view.bounds) + self.width/2.25, CGRectGetMidY(self.view.bounds) - self.height/2, self.width, self.height);
+    CGRect frame = CGRectMake(CGRectGetMidX(self.view.bounds) + width/2, CGRectGetMidY(self.view.bounds) - height/2, width, height);
     
     self.edgeView = [[UIView alloc] initWithFrame:frame];
     self.edgeView.backgroundColor = [UIColor greenColor];
+    self.edgeViewOldMidX = self.edgeView.center.x;
     [self.view addSubview:self.edgeView];
     
     self.edgePanGesture = [[UIScreenEdgePanGestureRecognizer alloc] initWithTarget:self action:@selector(viewEdgePan:)];
     self.edgePanGesture.edges = UIRectEdgeRight;
-    [self.view addGestureRecognizer:self.edgePanGesture];
+    [self.edgeView addGestureRecognizer:self.edgePanGesture];
     
-    self.swipeRightGesture = [[UIPanGestureRecognizer alloc] initWithTarget:self action:@selector(panned:)];
-    [self.view addGestureRecognizer:self.swipeRightGesture];
-    self.swipeRightGesture.enabled = NO;
+    self.panGesture = [[UIPanGestureRecognizer alloc] initWithTarget:self action:@selector(panned:)];
+    [self.edgeView addGestureRecognizer:self.panGesture];
+    self.panGesture.enabled = NO;
 }
 
 - (void)viewEdgePan:(UIScreenEdgePanGestureRecognizer *)sender
 {
-    CGPoint location = [sender locationInView:self.view]; // Location of where you pressed
-    CGPoint translation = [sender translationInView:self.view];
+//    CGPoint location = [sender locationInView:self.view]; // Location of where you pressed
+    CGPoint translation = [sender translationInView:sender.view];
+    sender.view.center = CGPointMake(sender.view.center.x + translation.x, sender.view.center.y);
+    [sender setTranslation:CGPointZero inView:sender.view];
     
     switch (sender.state)
     {
-//        case UIGestureRecognizerStateBegan:
-//            self.edgeViewGestureStartCenter = self.view.center;
-        case UIGestureRecognizerStateChanged:
-//            self.edgeView.center = center + xTranslation
-            if (location.x >= self.view.frame.size.width-100.0)
-            {
-                self.edgeView.center = CGPointMake(self.edgeView.center.x + translation.x, self.edgeView.center.y);
-                [sender setTranslation:CGPointMake(0, 0) inView:sender.view.superview];
-                self.edgePanGesture.enabled = NO;
-                self.swipeRightGesture.enabled = YES;
-            }
-            else if (translation.x > 0)
-            {
-                self.edgeView.center = CGPointMake(self.edgeView.center.x + translation.x, self.edgeView.center.y);
-                [sender setTranslation:CGPointMake(0, 0) inView:sender.view.superview];
-            }
-            break;
         case UIGestureRecognizerStateEnded:
-            if (location.x > 290)
+            if (sender.view.center.x >= self.edgeViewOldMidX - 100.0)
             {
                 [UIView animateWithDuration:0.5 animations:^{
-                    
-                    self.width = 400;
-                    self.height = 400;
-                    
-                    self.edgeView.frame = CGRectMake(CGRectGetMidX(self.view.bounds) + self.width/2.25, CGRectGetMidY(self.view.bounds) - self.height/2, self.width, self.height);
+                    sender.view.center = CGPointMake(self.edgeViewOldMidX, sender.view.center.y);
                 }];
+                break;
             }
             else
             {
                 [UIView animateWithDuration:0.5 animations:^{
-                    self.edgeView.frame = CGRectMake(self.view.frame.size.width*0.5, self.edgeView.frame.origin.y, self.edgeView.frame.size.width, self.edgeView.frame.size.height);
+                    sender.view.center = CGPointMake(self.view.center.x + 50, sender.view.center.y);
                 }];
+                self.edgeViewNewMidX = sender.view.center.x;
+                [sender.view removeGestureRecognizer:sender];
+                
+                self.edgePanGesture.enabled = NO;
+                self.panGesture.enabled = YES;
+
+                break;
             }
-            break;
-            
         default:
             break;
     }
@@ -91,14 +80,36 @@
 
 - (void)panned:(UIPanGestureRecognizer *)sender
 {
-    [UIView animateWithDuration:0.5 animations:^{
-        CGFloat width = 400;
-        CGFloat height = 400;
-        
-        self.edgeView.frame = CGRectMake(CGRectGetMidX(self.view.frame) + width/2, CGRectGetMidY(self.view.frame) - height/2, width, height);
-        }];
-    self.swipeRightGesture.enabled = NO;
-    self.edgePanGesture.enabled = YES;
+    CGPoint translation = [sender translationInView:sender.view];
+    sender.view.center = CGPointMake(sender.view.center.x + translation.x, sender.view.center.y);
+    [sender setTranslation:CGPointZero inView:sender.view];
+    
+    switch (sender.state)
+    {
+        case UIGestureRecognizerStateEnded:
+            if (sender.view.center.x < self.edgeViewNewMidX + 100)
+            {
+                [UIView animateWithDuration:0.5 animations:^{
+                    sender.view.center = CGPointMake(self.edgeViewNewMidX, sender.view.center.y);
+                }];
+                break;
+            }
+            else
+            {
+                [UIView animateWithDuration:0.5 animations:^{
+                    sender.view.center = CGPointMake(self.edgeViewOldMidX, sender.view.center.y);
+                }];
+                [sender.view removeGestureRecognizer:sender];
+                
+                self.panGesture.enabled = NO;
+                self.edgePanGesture.enabled = YES;
+                
+                break;
+            }
+            
+        default:
+            break;
+    }
 }
 
 @end
